0d83b0aef50c68b5b0c07e5d9a7208b9bd1a038b
declaresub <charles@declaresub.com>
2023-07-11 15:47:58 -0400

    Reformat code.

--- tests/test_resource_id.py
+++ tests/test_resource_id.py
@@ -1,13 +1,13 @@
 from typing import Union
+from uuid import UUID
 
 import pytest
-from uuid import UUID
 
 from resource_id import ResourceId
 from resource_id.resource_id import Base62Encodable
 
 
-@pytest.mark.parametrize('arg', [1, 'test', UUID(int=1728)])
+@pytest.mark.parametrize("arg", [1, "test", UUID(int=1728)])
 def test_init(arg: Union[str, Base62Encodable]):
     assert ResourceId(arg)
 
@@ -16,9 +16,11 @@ def test_init_bad_value():
     with pytest.raises(ValueError):
         ResourceId(-1)
 
+
 def test_init_bad_type():
     with pytest.raises(TypeError):
-        ResourceId(None) # type: ignore
+        ResourceId(None)  # type: ignore
+
 
 def test_bad_value_after_init():
     res_id = ResourceId(1)
@@ -26,28 +28,34 @@ def test_bad_value_after_init():
     with pytest.raises(ValueError):
         repr(res_id)
 
+
 def test_repr():
     arg = 1
-    assert repr(ResourceId(arg)) == '1'
+    assert repr(ResourceId(arg)) == "1"
 
 
 def test_invalid_base62():
     with pytest.raises(ValueError):
         ResourceId("bad-value")
 
+
 def test_uuid():
     value = UUID(int=123)
     assert ResourceId(value).uuid == value
 
+
 def test_eq():
     assert ResourceId(1) == ResourceId(1)
+
 
 def test_eq_class_mismatch():
     with pytest.raises(TypeError):
         ResourceId(1) != 1  # type: ignore
 
+
 def test_hash():
     assert hash(ResourceId(1))
+
 
 def test_int():
     value = 2
@@ -57,12 +65,13 @@ def test_int():
 def test___get_validators__():
     assert next(ResourceId.__get_validators__()) == ResourceId.validate
 
-@pytest.mark.parametrize('arg', [1, 'test', UUID(int=1728)])
+
+@pytest.mark.parametrize("arg", [1, "test", UUID(int=1728)])
 def test_validate(arg: Union[str, Base62Encodable]):
     assert ResourceId.validate(arg)
+
 
 def test___modify_schema__():
     field_schema = {}
     ResourceId.__modify_schema__(field_schema)
-    assert field_schema['title'] == ResourceId.__name__
-
+    assert field_schema["title"] == ResourceId.__name__
